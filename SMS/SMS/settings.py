0f34820801c9120ea68f2cbdefa6da57573466e4
meghlesh <meghachaudhari24@gmail.com>
2026-06-11 15:13:54 +0530

    Update allowed hosts and CSRF trusted origins

--- SMS/SMS/settings.py
+++ SMS/SMS/settings.py
@@ -29,14 +29,14 @@ ALLOWED_HOSTS = [
     "api-sms-be-cddgg8d5euh3ckhg.southindia-01.azurewebsites.net",
     "cwsinventory.in",
     "www.cwsinventory.in",
-     "purple-desert-08b7f5700.7.azurestaticapps.net"
+     "green-plant-0186f8300.7.azurestaticapps.net"
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://api-sms-be-cddgg8d5euh3ckhg.southindia-01.azurewebsites.net",
     "https://cwsinventory.in",
     "https://www.cwsinventory.in",
-    "https://purple-desert-08b7f5700.7.azurestaticapps.net"
+    "https://green-plant-0186f8300.7.azurestaticapps.net"
 ]
 
 
